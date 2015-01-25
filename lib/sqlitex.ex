0275bbfac4aef4b34995c847fa8500417d626348
hqmq <michael@riesd.com>
2015-01-25 16:12:31 -0700

    first test passing

--- lib/sqlitex.ex
+++ lib/sqlitex.ex
@@ -1,2 +1,18 @@
 defmodule Sqlitex do
+  def close(db) do
+    :esqlite3.close(db)
+  end
+
+  def open(path) do
+    :esqlite3.open(path)
+  end
+
+  def query(db, query) do
+    {:ok, statement} = :esqlite3.prepare(query, db)
+    columns = :esqlite3.column_names(statement) |> Tuple.to_list
+    rows = :esqlite3.fetchall(statement)
+    Enum.map(rows, fn(row) ->
+      Enum.zip(columns,Tuple.to_list(row))
+    end)
+  end
 end
--- test/sqlitex_test.exs
+++ test/sqlitex_test.exs
@@ -1,7 +1,10 @@
 defmodule SqlitexTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "a basic query returns maps" do
+    {:ok, db} = Sqlitex.open('test/fixtures/golfscores.sqlite3')
+    [row] = db |> Sqlitex.query("SELECT * FROM players ORDER BY id LIMIT 1")
+    assert row == [id: 1, name: "Mikey", created_at: "2012-10-14 05:46:28.318107", updated_at: "2013-09-06 22:29:36.610911", type: :undefined]
+    Sqlitex.close(db)
   end
 end
