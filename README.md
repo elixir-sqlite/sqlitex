e3d2ebb10bf0498a992b06886cfe2fb6fbf0ab8e
hqmq <michael@riesd.com>
2015-01-25 18:01:09 -0700

    update README and package info

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Sqlitex
 =======
 
 An Elixir wrapper around [https://github.com/mmzeeman/esqlite](esqlite). The main aim here is to provide convenient usage of sqlite databases.
+
+Plans
+=====
+
+This package currently only supports really basic reads. I am building this package as an attempt to learn more about Elixir and I'll be adding features only as I need them.
+
+I would love to get any feedback I can on how other people might want to use SQlite with Elixir.
--- mix.exs
+++ mix.exs
@@ -5,28 +5,28 @@ defmodule Sqlitex.Mixfile do
     [app: :sqlitex,
      version: "0.0.1",
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps,
+     package: package,
+     description: """
+      A thin Elixir wrapper around esqlite
+    """]
   end
 
   # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
   # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:esqlite, git: "https://github.com/mmzeeman/esqlite.git", tag: "9967ced039246f75f66a3891f584b1f150e56463"}
     ]
+  end
+
+  defp package do
+   [contributors: ["Michael Ries"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/mmmries/sqlitex"}]
   end
 end
