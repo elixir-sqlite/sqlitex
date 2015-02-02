6e9e2c9e75d94ba3f2ca8b585d405bb0d70086be
hqmq <michael@riesd.com>
2015-02-01 23:50:05 -0700

    typecast based on column type

--- lib/sqlitex.ex
+++ lib/sqlitex.ex
@@ -9,21 +9,22 @@ defmodule Sqlitex do
 
   def query(db, query, opts \\ []) do
     {:ok, statement} = :esqlite3.prepare(query, db)
-    columns = :esqlite3.column_names(statement)
+    types = :esqlite3.column_types(statement) |> Tuple.to_list
+    columns = :esqlite3.column_names(statement) |> Tuple.to_list
     rows = :esqlite3.fetchall(statement)
     into = Keyword.get(opts, :into, [])
 
     for row <- rows do
-      build_row(columns, row, into)
+      build_row(types, columns, row, into)
     end
   end
 
-  defp build_row(columns, row, into) do
-    values = row |> Tuple.to_list |> Enum.map(&translate_value/1)
+  defp build_row(types, columns, row, into) do
+    values = row |> Tuple.to_list |> Enum.zip(types) |> Enum.map(&translate_value/1)
 
-    Tuple.to_list(columns)
-    |> Enum.zip(values)
-    |> Enum.into(into)
+    columns
+      |> Enum.zip(values)
+      |> Enum.into(into)
   end
 
   def with_db(path, fun) do
@@ -33,19 +34,16 @@ defmodule Sqlitex do
     res
   end
 
-  defp translate_value(str) when is_binary(str) do
-    case Regex.run(~r<\A(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}).\d{6}\Z>, str) do
-      [_s,yrs,mos,das,hrs,mns,ss] ->
-        [yr,mo,da,hr,mn,s] = [yrs,mos,das,hrs,mns,ss] |> Enum.map(&String.to_integer/1)
-        {{yr,mo,da},{hr,mn,s}}
-      nil ->
-        str
-    end
+  defp translate_value({str, :datetime}) do
+    <<yr::binary-size(4), "-", mo::binary-size(2), "-", da::binary-size(2), " ", hr::binary-size(2), ":", mi::binary-size(2), ":", se::binary-size(2), ".", _fr::binary-size(6)>> = str
+    {{String.to_integer(yr), String.to_integer(mo), String.to_integer(da)},{String.to_integer(hr), String.to_integer(mi), String.to_integer(se)}}
   end
-  defp translate_value(:undefined) do
+
+  defp translate_value({:undefined,_type}) do
     nil
   end
-  defp translate_value(val) do
+
+  defp translate_value({val, _type}) do
     val
   end
 end
