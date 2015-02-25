69caa9d82924238475e7667abf932248fe9a1695
hqmq <michael@riesd.com>
2015-02-24 21:21:28 -0700

    add travis badge [ci skip]

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sqlitex.Mixfile do
 
   def project do
     [app: :sqlitex,
-     version: "0.0.6",
+     version: "0.0.7",
      elixir: "~> 1.0",
      deps: deps,
      package: package,
