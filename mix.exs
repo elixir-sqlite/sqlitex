7b577b7e759f8da1057bc25252bf7084565f10ab
hqmq <michael@riesd.com>
2015-02-24 21:19:15 -0700

    update to require esqlite 0.0.4

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Sqlitex.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:esqlite, "~> 0.0.3"}
+      {:esqlite, "~> 0.0.4"}
     ]
   end
 
