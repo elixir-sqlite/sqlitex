c1a90f636c71d38d8d253a1bea93564d60f6d397
jazzyb <json.barnes@gmail.com>
2015-04-17 15:13:44 -0400

    use in-memory database for tests
    This commit converts the old fixture database into an in-memory database for
    unit tests.

--- test/sqlitex_test.exs
+++ test/sqlitex_test.exs
@@ -1,40 +1,41 @@
 defmodule SqlitexTest do
   use ExUnit.Case
 
-  test "a basic query returns a list of keyword lists" do
-    {:ok, db} = Sqlitex.open('test/fixtures/golfscores.sqlite3')
-    [row] = db |> Sqlitex.query("SELECT * FROM players ORDER BY id LIMIT 1")
-    assert row == [id: 1, name: "Mikey", created_at: {{2012,10,14},{05,46,28}}, updated_at: {{2013,09,06},{22,29,36}}, type: nil]
-    Sqlitex.close(db)
+  setup_all do
+    {:ok, db} = Sqlitex.open(':memory:')
+    on_exit fn ->
+      Sqlitex.close(db)
+    end
+    {:ok, golf_db: TestDatabase.init(db)}
   end
 
-  test "a basic query returns a list of maps when into: %{} is given" do
-    {:ok, db} = Sqlitex.open('test/fixtures/golfscores.sqlite3')
-    [row] = db |> Sqlitex.query("SELECT * FROM players ORDER BY id LIMIT 1", into: %{})
+  test "a basic query returns a list of keyword lists", context do
+    [row] = context[:golf_db] |> Sqlitex.query("SELECT * FROM players ORDER BY id LIMIT 1")
+    assert row == [id: 1, name: "Mikey", created_at: {{2012,10,14},{05,46,28}}, updated_at: {{2013,09,06},{22,29,36}}, type: nil]
+  end
+
+  test "a basic query returns a list of maps when into: %{} is given", context do
+    [row] = context[:golf_db] |> Sqlitex.query("SELECT * FROM players ORDER BY id LIMIT 1", into: %{})
     assert row == %{id: 1, name: "Mikey", created_at: {{2012,10,14},{05,46,28}}, updated_at: {{2013,09,06},{22,29,36}}, type: nil}
-    Sqlitex.close(db)
   end
 
   test "with_db" do
-    [row] = Sqlitex.with_db('test/fixtures/golfscores.sqlite3', fn(db) ->
+    [row] = Sqlitex.with_db(':memory:', fn(db) ->
+      db = TestDatabase.init(db)
       Sqlitex.query(db, "SELECT * FROM players ORDER BY id LIMIT 1")
     end)
 
     assert row == [id: 1, name: "Mikey", created_at: {{2012,10,14},{05,46,28}}, updated_at: {{2013,09,06},{22,29,36}}, type: nil]
   end
 
-  test "a parameterized query" do
-    {:ok, db} = Sqlitex.open('test/fixtures/golfscores.sqlite3')
-    [row] = db |> Sqlitex.query("SELECT id, name FROM players WHERE name LIKE ?1 AND type == ?2", bind: ["s%", "Team"])
+  test "a parameterized query", context do
+    [row] = context[:golf_db] |> Sqlitex.query("SELECT id, name FROM players WHERE name LIKE ?1 AND type == ?2", bind: ["s%", "Team"])
     assert row == [id: 25, name: "Slothstronauts"]
-    Sqlitex.close(db)
   end
 
-  test "a parameterized query into %{}" do
-    {:ok, db} = Sqlitex.open('test/fixtures/golfscores.sqlite3')
-    [row] = db |> Sqlitex.query("SELECT id, name FROM players WHERE name LIKE ?1 AND type == ?2", bind: ["s%", "Team"], into: %{})
+  test "a parameterized query into %{}", context do
+    [row] = context[:golf_db] |> Sqlitex.query("SELECT id, name FROM players WHERE name LIKE ?1 AND type == ?2", bind: ["s%", "Team"], into: %{})
     assert row == %{id: 25, name: "Slothstronauts"}
-    Sqlitex.close(db)
   end
 
   test "exec" do
