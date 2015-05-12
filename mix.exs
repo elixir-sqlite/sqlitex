b3db312ed6b9c7be6430393b01a98f97cf55fdbc
hqmq <michael@riesd.com>
2015-05-12 09:22:33 -0600

    bump 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sqlitex.Mixfile do
 
   def project do
     [app: :sqlitex,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.0",
      deps: deps,
      package: package,
