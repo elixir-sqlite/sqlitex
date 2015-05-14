c4153ee791d770ad730a39b8a72fff695521c7fe
hqmq <michael@riesd.com>
2015-05-13 21:26:33 -0600

    put public interface at the top of the sql builder

--- lib/sqlitex/sql_builder.ex
+++ lib/sqlitex/sql_builder.ex
@@ -3,29 +3,35 @@ defmodule Sqlitex.SqlBuilder do
   This module contains functions for SQL creation. At the moment
   it is only used for `CREATE TABLE` statements.
   """
+
+  # Returns an SQL CREATE TABLE statement as a string. `name` is the name of the
+  # table, and `table_opts` contains the table constraints (at the moment only
+  # PRIMARY KEY is supported). `cols` is expected to be a keyword list in the
+  # form of:
+  #
+  # column_name: :column_type, of
+  # column_name: {:column_type, [column_constraints]}
+  def create_table(name, table_opts, cols) do
+    tbl_options = get_opts_dict(table_opts, &table_opt/1)
+    get_opt = &(Dict.get(tbl_options, &1, nil))
+
+    "CREATE #{get_opt.(:temp)} TABLE #{get_opt.(:if_not_exists)} #{name} (#{get_columns_block(cols)} #{get_opt.(:primary_key)})"
+  end
   
   # Supported table options
   defp table_opt(:temporary), do: {:temp, "TEMP"}
   defp table_opt(:temp), do: {:temp, "TEMP"}
-
   defp table_opt({:primary_key, cols}) when is_list(cols) do
     {:primary_key, ",PRIMARY KEY (" <> Enum.join(cols, ",") <> ")"}
   end
-
   defp table_opt({:primary_key, col}) when is_atom(col) do
     {:primary_key, ",PRIMARY KEY (" <> Atom.to_string(col) <> ")"}
   end
-
-  # Catch invalid / unsupported table options
-  defp table_opt(opt), do: raise "Unkown table option: #{IO.inspect(opt)}"
 
   # Supported column options
   defp column_opt(:primary_key), do: {:primary_key, "PRIMARY KEY"}
   defp column_opt(:not_null), do: {:not_null, "NOT NULL"}
   defp column_opt(:autoincrement), do: {:autoincrement, "AUTOINCREMENT"}
-
-  # Catch invalid / unsupported column options
-  defp column_opt(opt), do: raise "Unknown column option: #{IO.inspect(opt)}"
 
   # Helper function that creates a dictionary of option names
   # and their string representations
@@ -56,19 +62,5 @@ defmodule Sqlitex.SqlBuilder do
           "#{acc}#{comma} #{name} #{type}"
       end
     end)
-  end
-
-  # Returns an SQL CREATE TABLE statement as a string. `name` is the name of the
-  # table, and `table_opts` contains the table constraints (at the moment only
-  # PRIMARY KEY is supported). `cols` is expected to be a keyword list in the
-  # form of:
-  #
-  # column_name: :column_type, of
-  # column_name: {:column_type, [column_constraints]}
-  def create_table(name, table_opts, cols) do
-    tbl_options = get_opts_dict(table_opts, &table_opt/1)
-    get_opt = &(Dict.get(tbl_options, &1, nil))
-
-    "CREATE #{get_opt.(:temp)} TABLE #{get_opt.(:if_not_exists)} #{name} (#{get_columns_block(cols)} #{get_opt.(:primary_key)})"
   end
 end
