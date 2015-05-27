9207bc7d95a13fe36af382fc6a9ce4bea485b668
jazzyb <json.barnes@gmail.com>
2015-05-26 20:39:15 -0400

    adds datetime conversions

--- lib/sqlitex.ex
+++ lib/sqlitex.ex
@@ -71,8 +71,22 @@ defmodule Sqlitex do
       nil -> :undefined
       true -> 1
       false -> 0
+      {{_yr, _mo, _da}, {_hr, _mi, _se}}=datetime -> datetime_to_string(datetime)
+      {{yr, mo, da}, {hr, mi, se, _usecs}} -> datetime_to_string({{yr, mo, da}, {hr, mi, se}})
       other -> other
     end)
+  end
+
+  # Translate the given Erlang datetime tuple to an appropriate string for
+  # SQLite.  Microseconds are zeroed.
+  defp datetime_to_string({{yr, mo, da}, {hr, mi, se}}) do
+    [zero_pad(yr, 4), "-", zero_pad(mo, 2), "-", zero_pad(da, 2), " ", zero_pad(hr, 2), ":", zero_pad(mi, 2), ":", zero_pad(se, 2), ".000000"]
+    |> Enum.join
+  end
+
+  defp zero_pad(num, len) do
+    str = Integer.to_string num
+    String.duplicate("0", len - String.length(str)) <> str
   end
 
   defp return_rows_or_error(_, _, {:error, _} = error, _), do: error
