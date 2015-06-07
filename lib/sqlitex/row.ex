ba9513f294956d3f994eab24fecfa22c496114da
jazzyb <json.barnes@gmail.com>
2015-06-07 15:55:16 -0400

    handles precision and scale in Decimal type
    closes #18

--- lib/sqlitex/row.ex
+++ lib/sqlitex/row.ex
@@ -30,7 +30,17 @@ defmodule Sqlitex.Row do
   defp translate_value({0, "boolean"}), do: false
   defp translate_value({1, "boolean"}), do: true
 
+  defp translate_value({int, type=<<"decimal", _::binary>>}) when is_integer(int) do
+    {result, _} = int |> Integer.to_string |> Float.parse
+    translate_value({result, type})
+  end
   defp translate_value({float, "decimal"}), do: Decimal.new(float)
+  defp translate_value({float, "decimal(" <> rest}) do
+    [precision, scale] = rest |> String.rstrip(?)) |> String.split(",") |> Enum.map(&String.to_integer/1)
+    Decimal.with_context %Decimal.Context{precision: precision, rounding: :down}, fn ->
+      float |> Float.round(scale) |> Decimal.new |> Decimal.plus
+    end
+  end
 
   defp translate_value({val, _type}) do
     val
