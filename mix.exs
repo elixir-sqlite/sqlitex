2f0195777316e940f5d5c4b0037fa93cb57f4d88
hqmq <michael@riesd.com>
2015-06-16 21:49:33 -0600

    v0.6.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sqlitex.Mixfile do
 
   def project do
     [app: :sqlitex,
-     version: "0.6.0",
+     version: "0.6.1",
      elixir: "~> 1.0",
      deps: deps,
      package: package,
