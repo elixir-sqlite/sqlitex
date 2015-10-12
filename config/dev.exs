38b3cefbe6ad11df1cb9f565c9a15cb6c694e62e
Michael Ries <michael@riesd.com>
2015-10-12 13:26:56 -0600

    configure our own RuleSet

--- config/dev.exs
+++ config/dev.exs
@@ -1,11 +1,41 @@
 use Mix.Config
 
-config :dogma,
-  rule_set: Dogma.RuleSet.All,
+defmodule Sqlitex.DogmaRuleSet do
+  @behaviour Dogma.RuleSet
+  def rules do
+    [
+      {BarePipeChainStart},
+      {ComparisonToBoolean},
+      {DebuggerStatement},
+      {ExceptionName},
+      {FinalCondition},
+      {FinalNewline},
+      {FunctionArity, max: 4},
+      {FunctionName},
+      {HardTabs},
+      {LineLength, max_length: 100},
+      {LiteralInCondition},
+      {LiteralInInterpolation},
+      {MatchInCondition},
+      {ModuleAttributeName},
+      {ModuleDoc},
+      {ModuleName},
+      {NegatedIfUnless},
+      {PredicateName},
+      {QuotesInString},
+      {Semicolon},
+      {TrailingBlankLines},
+      {TrailingWhitespace},
+      {UnlessElse},
+      {VariableName},
+      {WindowsLineEndings},
+    ]
+  end
+end
 
+
+config :dogma,
+  rule_set: Sqlitex.DogmaRuleSet,
   exclude: [
     ~r(\Atest/test_database.exs),
-  ],
-  additional_config: [
-    LineLength: [max_length: 100]
   ]
