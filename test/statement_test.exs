212bb0fa0aacd12c37787afbc751927f2ff55672
Eric Scouten <eric@scouten.com>
2017-03-15 18:01:14 -0700

    Sigh. Can't use describe blocks in Elixir 1.2.x.

--- test/statement_test.exs
+++ test/statement_test.exs
@@ -12,53 +12,51 @@ defmodule StatementTest do
     assert result == [[user_version: 0]]
   end
 
-  describe "RETURNING pseudo-syntax" do
-    test "returns id from a single row insert" do
-      {:ok, db} = Sqlitex.open(":memory:")
+  test "RETURNING pseudo-syntax returns id from a single row insert" do
+    {:ok, db} = Sqlitex.open(":memory:")
 
-      Sqlitex.exec(db, "CREATE TABLE x(id INTEGER PRIMARY KEY AUTOINCREMENT, str)")
+    Sqlitex.exec(db, "CREATE TABLE x(id INTEGER PRIMARY KEY AUTOINCREMENT, str)")
 
-      stmt = Sqlitex.Statement.prepare!(db, "INSERT INTO x(str) VALUES (?1) "
-                                            <> ";--RETURNING ON INSERT x,id")
+    stmt = Sqlitex.Statement.prepare!(db, "INSERT INTO x(str) VALUES (?1) "
+                                          <> ";--RETURNING ON INSERT x,id")
 
-      rows = Sqlitex.Statement.fetch_all!(stmt)
-      assert rows == [[id: 1]]
-    end
+    rows = Sqlitex.Statement.fetch_all!(stmt)
+    assert rows == [[id: 1]]
+  end
 
-    test "returns id from a single row insert as a raw list" do
-      {:ok, db} = Sqlitex.open(":memory:")
+  test "RETURNING pseudo-syntax returns id from a single row insert as a raw list" do
+    {:ok, db} = Sqlitex.open(":memory:")
 
-      Sqlitex.exec(db, "CREATE TABLE x(id INTEGER PRIMARY KEY AUTOINCREMENT, str)")
+    Sqlitex.exec(db, "CREATE TABLE x(id INTEGER PRIMARY KEY AUTOINCREMENT, str)")
 
-      stmt = Sqlitex.Statement.prepare!(db, "INSERT INTO x(str) VALUES (?1) "
-                                            <> ";--RETURNING ON INSERT x,id")
+    stmt = Sqlitex.Statement.prepare!(db, "INSERT INTO x(str) VALUES (?1) "
+                                          <> ";--RETURNING ON INSERT x,id")
 
-      rows = Sqlitex.Statement.fetch_all!(stmt, :raw_list)
-      assert rows == [[1]]
-    end
+    rows = Sqlitex.Statement.fetch_all!(stmt, :raw_list)
+    assert rows == [[1]]
+  end
 
-    test "returns id from a multi-row insert" do
-      {:ok, db} = Sqlitex.open(":memory:")
+  test "RETURNING pseudo-syntax returns id from a multi-row insert" do
+    {:ok, db} = Sqlitex.open(":memory:")
 
-      Sqlitex.exec(db, "CREATE TABLE x(id INTEGER PRIMARY KEY AUTOINCREMENT, str)")
+    Sqlitex.exec(db, "CREATE TABLE x(id INTEGER PRIMARY KEY AUTOINCREMENT, str)")
 
-      stmt = Sqlitex.Statement.prepare!(db, "INSERT INTO x(str) VALUES ('x'),('y'),('z') "
-                                            <> ";--RETURNING ON INSERT x,id")
+    stmt = Sqlitex.Statement.prepare!(db, "INSERT INTO x(str) VALUES ('x'),('y'),('z') "
+                                          <> ";--RETURNING ON INSERT x,id")
 
-      rows = Sqlitex.Statement.fetch_all!(stmt)
-      assert rows == [[id: 1], [id: 2], [id: 3]]
-    end
+    rows = Sqlitex.Statement.fetch_all!(stmt)
+    assert rows == [[id: 1], [id: 2], [id: 3]]
+  end
 
-    test "returns id from a multi-row insert as a raw list" do
-      {:ok, db} = Sqlitex.open(":memory:")
+  test "RETURNING pseudo-syntax returns id from a multi-row insert as a raw list" do
+    {:ok, db} = Sqlitex.open(":memory:")
 
-      Sqlitex.exec(db, "CREATE TABLE x(id INTEGER PRIMARY KEY AUTOINCREMENT, str)")
+    Sqlitex.exec(db, "CREATE TABLE x(id INTEGER PRIMARY KEY AUTOINCREMENT, str)")
 
-      stmt = Sqlitex.Statement.prepare!(db, "INSERT INTO x(str) VALUES ('x'),('y'),('z') "
-                                            <> ";--RETURNING ON INSERT x,id")
+    stmt = Sqlitex.Statement.prepare!(db, "INSERT INTO x(str) VALUES ('x'),('y'),('z') "
+                                          <> ";--RETURNING ON INSERT x,id")
 
-      rows = Sqlitex.Statement.fetch_all!(stmt, :raw_list)
-      assert rows == [[1], [2], [3]]
-    end
+    rows = Sqlitex.Statement.fetch_all!(stmt, :raw_list)
+    assert rows == [[1], [2], [3]]
   end
 end
