3524ef4d32555d24f63f66e524e407f802cf5443
Michael Ries <michael@riesd.com>
2017-03-15 20:17:00 -0600

    release 1.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sqlitex.Mixfile do
 
   def project do
     [app: :sqlitex,
-     version: "1.1.1",
+     version: "1.2.0",
      elixir: "~> 1.2",
      deps: deps(),
      package: package(),
