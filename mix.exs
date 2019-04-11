f580bfe5b265d3988674d480a533bff8c4d14e9e
Connor Rigby <konnorrigby@gmail.com>
2019-04-11 11:01:07 -0700

    Release version 1.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sqlitex.Mixfile do
   def project do
     [
       app: :sqlitex,
-      version: "1.5.1",
+      version: "1.6.0",
       elixir: "~> 1.4",
       deps: deps(),
       package: package(),
